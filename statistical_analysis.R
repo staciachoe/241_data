ba1a4d1ee8fc4735af9e4a8a83b46d381a4e159f
chingnaw <icwang@berkeley.edu>
2017-12-12 17:18:10 -0800

    updated analysis

--- statistical_analysis.R
+++ statistical_analysis.R
@@ -9,6 +9,8 @@ d <- data.table(d)
 
 d$hours <- as.numeric(as.character(d$hours))
 
+d$hours <- na.omit(d$hours)
+
 ate <- d %>%
   .[ , .(m = mean(hours)), keyby = .(from)] %>%
   .[ , diff(m)]
@@ -24,3 +26,33 @@ summary(m2)
 
 anova(m1)
 anova(m2)
+
+#combine sector and sender
+m3<- lm(d$hours ~ d$sector + d$from)
+summary(m3)
+anova(m3)
+
+cl <- function(fm, cluster){
+  require(sandwich, quietly = TRUE)
+  require(lmtest, quietly = TRUE)
+  M <- length(unique(cluster))
+  N <- length(cluster)
+  K <- fm$rank
+  dfc <- (M/(M-1))*((N-1)/(N-K))
+  uj <- apply(estfun(fm),2, function(x) tapply(x, cluster, sum));
+  vcovCL <- dfc*sandwich(fm, meat=crossprod(uj)/N)
+  coeftest(fm, vcovCL)
+}
+
+cl(m3, d$from)
+
+cl(m3, d$from)[11]
+
+
+#combine sector and sender
+m4<- lm(d$hours ~ d$from + d$sector)
+summary(m4)
+anova(m4)
+
+cl(m4, d$sector)
+cl(m4, d$sector)[11]
